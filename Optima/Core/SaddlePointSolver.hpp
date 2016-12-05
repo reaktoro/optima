bd7e4f56c9023a143b589fd1dbbc5a42142dd34d
Allan Leal <allanmulin@gmail.com>
2016-12-05 14:52:53 +0100

    Updated copyright date

--- Optima/Core/SaddlePointSolver.hpp
+++ Optima/Core/SaddlePointSolver.hpp
@@ -1,6 +1,6 @@
 // Optima is a C++ library for numerical solution of linear and nonlinear programing problems.
 //
-// Copyright (C) 2014-2016 Allan Leal
+// Copyright (C) 2014-2017 Allan Leal
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
