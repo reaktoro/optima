b5e3df3e76e6f183a2841cdb6979b05c3221eaf8
Allan Leal <allanmulin@gmail.com>
2017-04-03 16:06:36 +0200

    Updated list of header files

--- Optima/Optima.hpp
+++ Optima/Optima.hpp
@@ -22,7 +22,6 @@
 #include <Optima/Common/Optional.hpp>
 #include <Optima/Common/Outputter.hpp>
 #include <Optima/Common/Result.hpp>
-#include <Optima/Common/SetUtils.hpp>
 #include <Optima/Common/Timing.hpp>
 #include <Optima/Core/OptimumOptions.hpp>
 #include <Optima/Core/OptimumParams.hpp>
@@ -44,25 +43,3 @@
 #include <Optima/Math/Matrix.hpp>
 #include <Optima/Math/Utils.hpp>
 
-#ifdef CLING
-#include <Optima/Common/Exception.cpp>
-#include <Optima/Common/Outputter.cpp>
-#include <Optima/Common/Result.cpp>
-#include <Optima/Common/Timing.cpp>
-#include <Optima/Core/OptimumOptions.cpp>
-#include <Optima/Core/OptimumParams.cpp>
-#include <Optima/Core/OptimumProblem.cpp>
-#include <Optima/Core/OptimumResult.cpp>
-#include <Optima/Core/OptimumSolver.cpp>
-#include <Optima/Core/OptimumStepper.cpp>
-#include <Optima/Core/OptimumStructure.cpp>
-#include <Optima/Core/SaddlePointMatrix.cpp>
-#include <Optima/Core/SaddlePointProblem.cpp>
-#include <Optima/Core/SaddlePointResult.cpp>
-#include <Optima/Core/SaddlePointSolver.cpp>
-#include <Optima/Core/SaddlePointUtils.cpp>
-#include <Optima/Math/BlockDiagonalMatrix.cpp>
-#include <Optima/Math/Canonicalizer.cpp>
-#include <Optima/Math/EigenExtern.cpp>
-#include <Optima/Math/Utils.cpp>
-#endif
