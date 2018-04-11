366768a06e4430ecf0a0a8e9c6ddfdc69d265db0
Allan Leal <allanmulin@gmail.com>
2018-04-11 13:21:48 +0200

    Implemented tests for stable partition functions

--- tests/Partition.py
+++ tests/Partition.py
@@ -19,13 +19,14 @@ from optima import *
 from numpy import *
 from pytest import approx
 
+
 def test_index_utils():
     n = 10
     
-    assert contains([1,3,6], 1)
-    assert contains([1,3,6], 3)
-    assert contains([1,3,6], 6)
-    assert not contains([1,3,6], 7)
+    assert contains([1, 3, 6], 1)
+    assert contains([1, 3, 6], 3)
+    assert contains([1, 3, 6], 6)
+    assert not contains([1, 3, 6], 7)
     
     inds = arange(n)
     partitionLeft(inds, [1, 3, 5, 7])
@@ -36,3 +37,13 @@ def test_index_utils():
     partitionRight(inds, [1, 3, 5, 7])
     assert set(inds[-4:]) == set([1, 3, 5, 7])
     assert set(inds[:-4]) == set([0, 2, 4, 6, 8, 9]) 
+    
+    inds = arange(n)
+    partitionLeftStable(inds, [1, 3, 5, 7])
+    assert inds[:4] == approx([1, 3, 5, 7])
+    assert inds[4:] == approx([0, 2, 4, 6, 8, 9]) 
+    
+    inds = arange(n)
+    partitionRightStable(inds, [1, 3, 5, 7])
+    assert inds[-4:] == approx([1, 3, 5, 7])
+    assert inds[:-4] == approx([0, 2, 4, 6, 8, 9]) 
