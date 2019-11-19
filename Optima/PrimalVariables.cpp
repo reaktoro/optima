ed1e1048851c77684697c36dc44c7a1962b7415d
Allan Leal <allan.leal@erdw.ethz.ch>
2019-11-19 11:22:25 +0100

    Implement PrimalVariables

---
+++ Optima/PrimalVariables.cpp
@@ -0,0 +1,76 @@
+// Optima is a C++ library for solving linear and non-linear constrained optimization problems
+//
+// Copyright (C) 2014-2018 Allan Leal
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#include "PrimalVariables.hpp"
+
+// Optima includes
+#include <Optima/Constraints.hpp>
+
+namespace Optima {
+
+PrimalVariables::PrimalVariables()
+{}
+
+PrimalVariables::PrimalVariables(const Constraints& constraints)
+: nx(constraints.numVariables()),
+  nxli(constraints.numLinearInequalityConstraints()),
+  nxni(constraints.numNonLinearInequalityConstraints()),
+  data(nx + nxli + nxni)
+{
+}
+
+auto PrimalVariables::canonicalPrimalVariables() const -> VectorConstRef
+{
+    return data;
+}
+
+auto PrimalVariables::canonicalPrimalVariables() -> VectorRef
+{
+    return data;
+}
+
+auto PrimalVariables::originalPrimalVariables() const -> VectorConstRef
+{
+    return data.head(nx);
+}
+
+auto PrimalVariables::originalPrimalVariables() -> VectorRef
+{
+    return data.head(nx);
+}
+
+auto PrimalVariables::slackVariablesLinearInequalityConstraints() const -> VectorConstRef
+{
+    return data.segment(nx, nxli);
+}
+
+auto PrimalVariables::slackVariablesLinearInequalityConstraints() -> VectorRef
+{
+    return data.segment(nx, nxli);
+}
+
+auto PrimalVariables::slackVariablesNonLinearInequalityConstraints() const -> VectorConstRef
+{
+    return data.tail(nxni);
+}
+
+auto PrimalVariables::slackVariablesNonLinearInequalityConstraints() -> VectorRef
+{
+    return data.tail(nxni);
+}
+
+} // namespace Optima
---
+++ Optima/PrimalVariables.hpp
@@ -0,0 +1,77 @@
+// Optima is a C++ library for solving linear and non-linear constrained optimization problems
+//
+// Copyright (C) 2014-2018 Allan Leal
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// Optima includes
+#include <Optima/Index.hpp>
+#include <Optima/Matrix.hpp>
+
+namespace Optima {
+
+// Forward declarations
+class Constraints;
+
+/// A type that describes the primal variables in a canonical optimization problem.
+class PrimalVariables
+{
+public:
+    /// Construct a default PrimalVariables instance.
+    PrimalVariables();
+
+    /// Construct a PrimalVariables instance with given constraints.
+    PrimalVariables(const Constraints& constraints);
+
+    /// Return the primal variables of the canonical optimization problem.
+    auto canonicalPrimalVariables() const -> VectorConstRef;
+
+    /// Return the primal variables of the canonical optimization problem.
+    auto canonicalPrimalVariables() -> VectorRef;
+
+    /// Return the primal variables of the original optimization problem.
+    auto originalPrimalVariables() const -> VectorConstRef;
+
+    /// Return the primal variables of the original optimization problem.
+    auto originalPrimalVariables() -> VectorRef;
+
+    /// Return the primal variables of the canonical optimization problem with respect to linear inequality constraints.
+    auto slackVariablesLinearInequalityConstraints() const -> VectorConstRef;
+
+    /// Return the primal variables of the canonical optimization problem with respect to linear inequality constraints.
+    auto slackVariablesLinearInequalityConstraints() -> VectorRef;
+
+    /// Return the primal variables of the canonical optimization problem with respect to non-linear inequality constraints.
+    auto slackVariablesNonLinearInequalityConstraints() const -> VectorConstRef;
+
+    /// Return the primal variables of the canonical optimization problem with respect to non-linear inequality constraints.
+    auto slackVariablesNonLinearInequalityConstraints() -> VectorRef;
+
+private:
+    /// The number of primal variables in the optimization problem
+    Index nx = 0;
+
+    /// The number of slack primal variables with respect to linear inequality constraints
+    Index nxli = 0;
+
+    /// The number of slack primal variables with respect to non-linear inequality constraints
+    Index nxni = 0;
+
+    /// The vector containing the primal variables of the canonical optimization problem.
+    Vector data;
+};
+
+} // namespace Optima
