19ece6339782ff08a20f2367da4f15eae6d61ccc
Allan Leal <allan.leal@erdw.ethz.ch>
2020-12-05 17:51:23 +0100

    Fix comment (replace unix by win)

--- cmake/CondaAware.cmake
+++ cmake/CondaAware.cmake
@@ -25,7 +25,7 @@ if(DEFINED ENV{CONDA_PREFIX})
         message(STATUS "CondaAware: PYTHON_EXECUTABLE=CONDA_PREFIX/bin/python (${PYTHON_EXECUTABLE})")
     endif()
 
-    # Set python executable to that in the conda environment (unix)
+    # Set python executable to that in the conda environment (win)
     if(WIN32 AND DEFINED ENV{CONDA_PYTHON_EXE})
         set(PYTHON_EXECUTABLE "$ENV{CONDA_PREFIX}\\python.exe")
         message(STATUS "CondaAware: PYTHON_EXECUTABLE=CONDA_PREFIX\\python.exe (${PYTHON_EXECUTABLE})")
