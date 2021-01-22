0828b7c579659ec72928b58e1f1d8e2857ab121d
Allan Leal <5825588+allanleal@users.noreply.github.com>
2021-01-22 10:27:29 +0100

    Implemented envs4debug.bat and envs4release.bat to initiate environment variables PATH and PYTHONPATH from build directory

---
+++ scripts/envs4debug.bat
@@ -0,0 +1,2 @@
+set PYTHONPATH=%CD%\python\package\installed\Debug\Lib\site-packages;%PYTHONPATH%
+set PATH=%CD%\Optima\Debug;%PATH%
---
+++ scripts/envs4release.bat
@@ -0,0 +1,2 @@
+set PYTHONPATH=%CD%\python\package\installed\Release\Lib\site-packages;%PYTHONPATH%
+set PATH=%CD%\Optima\Release;%PATH%
